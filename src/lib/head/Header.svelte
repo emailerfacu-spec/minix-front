1271248af55cac7ecbf44d4c114348733ce85dc4
fede <federico.nicolas.polidoro@gmail.com>
2025-11-13 22:16:24 -0300

    initial commit

---
+++ src/lib/head/Header.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { Button } from '../components/ui/button';
+	import {
+		DropdownMenu,
+		DropdownMenuContent,
+		DropdownMenuItem,
+		DropdownMenuTrigger
+	} from '../components/ui/dropdown-menu';
+	import ButtonTheme from './ButtonTheme.svelte';
+</script>
+
+<header class="border-b bg-background/95 backdrop-blur">
+	<div class="container ms-2 flex h-14 items-center">
+		<div class="mr-4 hidden md:flex">
+			<a href="/" class="mr-6 flex items-center space-x-2">
+				<span class="hidden font-bold sm:inline-block">Mini-X</span>
+			</a>
+			<ButtonTheme />
+			<nav class="flex items-center space-x-6 text-sm font-medium">
+				<a href="/" class="text-foreground transition-colors hover:text-foreground/80">Inicio</a>
+				<a href="/about" class="text-foreground/60 transition-colors hover:text-foreground/80"
+					>Acerca de</a
+				>
+				<a href="/contact" class="text-foreground/60 transition-colors hover:text-foreground/80"
+					>Contacto</a
+				>
+			</nav>
+		</div>
+		<div class="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+			<nav class="flex items-center space-x-2">
+				<DropdownMenu>
+					<DropdownMenuTrigger as={Button} variant="ghost" size="sm">Menu</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuItem>Perfil</DropdownMenuItem>
+						<DropdownMenuItem>Configuración</DropdownMenuItem>
+						<DropdownMenuItem>Cerrar sesión</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</nav>
+		</div>
+	</div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+	import Header from '@/head/Header.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+<ModeWatcher />
+<Header />
+{@render children()}
