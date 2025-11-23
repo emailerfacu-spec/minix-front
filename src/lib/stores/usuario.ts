949a90a1ba102860695389b6142ad03ac97dfb77
fede <federico.nicolas.polidoro@gmail.com>
2025-11-23 20:18:55 -0300

    añadida logica para refrescar los jwt

--- src/lib/stores/usuario.ts
+++ src/lib/stores/usuario.ts
@@ -1,16 +1,18 @@
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
 import type { Sesion } from '../../types';
+import { apiBase } from '@/stores/url';
+
+const { subscribe } = apiBase;
+let baseUrl: string = '';
+
+subscribe((value) => {
+	baseUrl = value;
+})();
 
 const initialValue = browser ? JSON.parse(localStorage.getItem('sesion') || 'null') : null;
 
 export const currentSesion = writable<Sesion | null>(initialValue);
-
-if (browser) {
-	currentSesion.subscribe((value) => {
-		localStorage.setItem('sesion', JSON.stringify(value));
-	});
-}
 
 export const sesionStore = {
 	subscribe: currentSesion.subscribe,
@@ -18,3 +20,40 @@ export const sesionStore = {
 	update: currentSesion.update,
 	reset: () => currentSesion.set(null)
 };
+
+if (browser) {
+	currentSesion.subscribe((value) => {
+		localStorage.setItem('sesion', JSON.stringify(value));
+	});
+}
+if (browser) {
+	const refreshAccessToken = async () => {
+		try {
+			const response = await fetch(baseUrl + '/api/auth/refresh', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				credentials: 'include'
+			});
+
+			if (response.ok) {
+				const data = await response.json();
+				currentSesion.update((sesion) => {
+					if (sesion) {
+						return { ...sesion, accessToken: data.accessToken };
+					}
+					return sesion;
+				});
+			} else {
+				console.error('Error refreshing token:', response.statusText);
+				currentSesion.set(null);
+			}
+		} catch (error) {
+			console.error('Error refreshing token:', error);
+			currentSesion.set(null);
+		}
+	};
+
+	setInterval(refreshAccessToken, 10 * 60 * 1000);
+}
