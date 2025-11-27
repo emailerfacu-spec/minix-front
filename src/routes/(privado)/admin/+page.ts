d38a223b12ecefc6d0a14e9dfd627e3a83cf50c4
fede <federico.nicolas.polidoro@gmail.com>
2025-11-27 20:18:10 -0300

    fetch de usuarios ya funcional junto con la ui de usurios

--- src/routes/(privado)/admin/+page.ts
+++ src/routes/(privado)/admin/+page.ts
@@ -4,8 +4,12 @@ import { redirect } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 import type { UserResponseDto } from '../../../types.js';
 
+<<<<<<< HEAD
 //para que solo se envie cuando se requiere
 export const prerender = true;
+=======
+export const ssr = true;
+>>>>>>> 354f43e (fetch de usuarios ya funcional junto con la ui de usurios)
 
 export async function load({}) {
 	const response = await fetch(get(apiBase) + '/api/admin/users', {
