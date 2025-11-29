3068280b82345cb19a2619b8a5726f700c3c3f49
fede <federico.nicolas.polidoro@gmail.com>
2025-11-29 19:11:33 -0300

    Añadida implementacion de resetear contraseña

--- src/lib/components/TablaUsuarios.svelte
+++ src/lib/components/TablaUsuarios.svelte
@@ -5,15 +5,40 @@
 	import TableHeader from '@/components/ui/table/table-header.svelte';
 	import TableRow from '@/components/ui/table/table-row.svelte';
 	import Table from '@/components/ui/table/table.svelte';
-	import type { UserResponseDto } from '../../../types';
+	import type { UserResponseDto } from '../../types';
+	import Button from './ui/button/button.svelte';
+	import KeyIcon from '@lucide/svelte/icons/key';
+	import { Tooltip } from './ui/tooltip';
+	import TooltipTrigger from './ui/tooltip/tooltip-trigger.svelte';
+	import TooltipContent from './ui/tooltip/tooltip-content.svelte';
+	import RecuperarContraseña from './admin/RecuperarContraseña.svelte';
+	import { Dialog } from './ui/dialog';
+	import DialogContent from './ui/dialog/dialog-content.svelte';
 
 	interface Props {
 		usuarios: UserResponseDto[];
 	}
 
 	let { usuarios }: Props = $props();
+
+	let open = $state(false);
+
+	//si ponia contraseña en español quedaba muy largo el nombre
+	let usuarioCambioPass: UserResponseDto | null = $state(null);
+
+	$effect(() => {
+		if (!open) {
+			usuarioCambioPass = null;
+		}
+	});
+
+	function handleCambiarContraseña(usuario: UserResponseDto) {
+		open = true;
+		usuarioCambioPass = usuario;
+	}
 </script>
 
+<!-- {$inspect(usuarios)} -->
 <Table>
 	<TableHeader>
 		<TableRow>
@@ -31,8 +56,20 @@
 				<TableCell>{usuario.displayName}</TableCell>
 				<TableCell>?</TableCell>
 				<TableCell>{usuario.createdAt.replace('Z', ' ').replace('T', ' | ')}</TableCell>
-				<TableCell>?</TableCell>
+				<TableCell class="flex gap-2">
+					<Tooltip>
+						<TooltipTrigger>
+							<Button onclick={() => handleCambiarContraseña(usuario)}><KeyIcon></KeyIcon></Button>
+						</TooltipTrigger>
+						<TooltipContent>
+							<p>Recuperar Contraseña</p>
+						</TooltipContent>
+					</Tooltip>
+					<Button></Button>
+					<Button></Button>
+				</TableCell>
 			</TableRow>
 		{/each}
 	</TableBody>
 </Table>
+<RecuperarContraseña bind:open usuario={usuarioCambioPass} />
--- src/routes/(privado)/admin/+page.svelte
+++ src/routes/(privado)/admin/+page.svelte
@@ -12,18 +12,22 @@
 	import TableCell from '@/components/ui/table/table-cell.svelte';
 	import { page } from '$app/state';
 	import TablaUsuarios from '@/components/TablaUsuarios.svelte';
+	import CardTitle from '@/components/ui/card/card-title.svelte';
+	import CardHeader from '@/components/ui/card/card-header.svelte';
 
 	let cargando = $state(true);
 </script>
 
 <div class="flex min-h-fit w-full items-center justify-center p-6 md:p-10">
 	<div class="w-full max-w-6xl">
-		<h1
-			class="mb-4 scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance underline"
-		>
-			Gestion Usuarios
-		</h1>
 		<Card class={page.data.error ? 'border-red-400' : ''}>
+			<CardHeader class="w-full">
+				<CardTitle class="rounded-full bg-accent-foreground/10">
+					<h1 class="mt-3 mb-4 scroll-m-20 text-center text-2xl font-extrabold tracking-tight">
+						Gestion Usuarios
+					</h1>
+				</CardTitle>
+			</CardHeader>
 			<CardContent>
 				{#if page.data.usuarios.length === 0}
 					{#if page.data.error}
