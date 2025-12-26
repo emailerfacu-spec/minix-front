1152459006b225b4ba224d52d33f450848206103
fede <federico.nicolas.polidoro@gmail.com>
2025-12-26 16:40:06 -0300

    bruh XSS

--- src/lib/components/CardPerfil.svelte
+++ src/lib/components/CardPerfil.svelte
@@ -92,7 +92,8 @@
 			</h1>
 			{#if usu.bio}
 				<p class="mt-4 rounded-4xl bg-accent p-4 text-center text-muted-foreground">
-					{@html usu.bio.replaceAll('\n', '<br>')}
+					{usu.bio.replaceAll('\n', '<br>')}
+					<!-- {@html usu.bio.replaceAll('\n', '<br>')} -->
 				</p>
 			{/if}
 		{:else}
