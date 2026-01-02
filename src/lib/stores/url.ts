7f68f61ce2aca5d8acecf566e0e698890e6b3968
fede <federico.nicolas.polidoro@gmail.com>
2026-01-02 18:58:15 -0300

    se cambio el back a azure

--- src/lib/stores/url.ts
+++ src/lib/stores/url.ts
@@ -1,6 +1,12 @@
 import { dev } from '$app/environment';
 import { readable } from 'svelte/store';
 
+// export const apiBase = readable(
+// 	dev ? 'http://localhost:5000' : 'https://minix-back-dsuk.onrender.com'
+// );
+
 export const apiBase = readable(
-	dev ? 'http://localhost:5000' : 'https://minix-back-dsuk.onrender.com'
+	dev
+		? 'http://localhost:5000'
+		: 'https://minix-back-3.salmonpebble-66858787.brazilsouth.azurecontainerapps.io'
 );
