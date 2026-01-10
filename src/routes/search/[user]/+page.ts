556c5405653c8e5022f804d569d137dc70d918a5
fede <federico.nicolas.polidoro@gmail.com>
2026-01-10 17:43:22 -0300

    hecho soporte para la busqueda de hashtags

--- src/routes/search/[user]/+page.ts
+++ src/routes/search/[user]/+page.ts
@@ -1,9 +1,15 @@
 import { busquedaUsuarios } from '@/hooks/busquedaUsuarios';
 import { error } from '@sveltejs/kit';
 import type { UserResponseDto } from '../../../types';
+import { busquedaHashtags } from '@/hooks/busquedaHashtags.js';
 
 export async function load({ params }) {
-	let usuarios: UserResponseDto[] = await busquedaUsuarios(params.user);
+	let req1 = busquedaUsuarios(params.user);
+	//usa el mismo param
+	let req2 = busquedaHashtags(params.user);
+
+	let [usuarios, htags] = await Promise.all([req1, req2]);
+
 	if (usuarios == null) {
 		return error(500, 'No se pudo alcanzar el servidor.');
 	}
@@ -11,5 +17,5 @@ export async function load({ params }) {
 	if (usuarios.length == 0) {
 		return error(404, 'No se encontraron usuarios que coinsidan con la busqueda.');
 	}
-	return { usuarios };
+	return { usuarios, htags };
 }
