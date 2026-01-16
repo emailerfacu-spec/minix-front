d89e2960d772be38d5d6254ade32c0fe0e0a65f3
fede <federico.nicolas.polidoro@gmail.com>
2026-01-16 01:59:28 -0300

    primer prueba de concepto para firebase

---
+++ src/lib/components/FireBaseButton.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { getFirebaseAuth } from '$lib/stores/firebase.ts';
+	import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+	import Button from './ui/button/button.svelte';
+
+	const handleGoogleSignIn = async () => {
+		const auth = getFirebaseAuth();
+		const provider = new GoogleAuthProvider();
+		try {
+			await signInWithPopup(auth, provider);
+		} catch (error) {
+			console.error('Error signing in with Google:', error);
+		}
+	};
+</script>
+
+<Button onclick={handleGoogleSignIn}>Sign in with Google</Button>
